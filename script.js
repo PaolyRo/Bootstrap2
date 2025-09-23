dcb7470275b8f6c9d27e6d3e08cb6a1af492284a
Romero Paola <paoly.alex.romero@gmail.com>
2025-09-23 09:51:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Aquí contraste
+const body = document.body;
+const modeToggle = document.getElementById('modeToggle');
+
+// Cargar modo guardado
+body.classList.toggle('dark-mode', localStorage.getItem('mode') === 'dark');
+
+modeToggle.addEventListener('click', () => {
+   const isDark = body.classList.toggle('dark-mode');
+   localStorage.setItem('mode', isDark ? 'dark' : 'light');
+});
